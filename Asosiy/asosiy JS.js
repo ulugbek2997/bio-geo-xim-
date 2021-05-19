300699da1f916ccd810c813cf3512b3284b86592
ulugbek2997 <rustamovulugbek831@gmail.com>
2021-05-19 16:25:53 +0500

    update version homePage

---
+++ Asosiy/asosiy JS.js
@@ -0,0 +1,147 @@
+// import {fetch, randomNumber,getCardInfo,createCard} from '../js functions/mainJS.js';
+
+// fetch();
+let newsArr = [];
+
+
+fetch('http://192.144.37.95:8080/api/articles?langId=1')
+.then((response) => {
+  return response.json();
+})
+.then((data) => {
+  console.log(data)
+
+// getCardInfo(randomNumber(data['length']),data)
+
+function createRowElemnts(){
+  for(i=0;i<3;i++){
+   getCardInfo(randomNumber(data['length']),data)
+   
+  }
+}
+
+createRowElemnts();
+
+//create papers row//
+
+for(i=0;i<3;i++){
+  createOfPaper(data['length'],data)
+}
+
+  return data
+});
+
+
+function getCardInfo(apiMainId,getApi){
+   let cardTitle = getApi[apiMainId]["title"];
+   let cardBody = getApi[apiMainId]['body'];
+   let cardMainCategoriy = getApi[apiMainId]['category']['name'];
+   
+
+   let date = new Date(getApi[apiMainId]['date']);
+   let day = date.getDay();
+   let month = date.getMonth();
+   let year =date.getFullYear(); 
+
+   let cardMainDate = day +'.'+month+'.'+year;
+
+    createCard(cardTitle,cardBody,cardMainCategoriy,cardMainDate)
+}
+
+
+ function createCard(cardTitle,cardBody,cardMainCategoriy,cardMainDate){
+    
+  let card= document.createElement('div');
+  card.classList.add('card')
+
+  let cardImg =document.createElement('div');
+  cardImg.classList.add('card-img');
+  
+  let category= document.createElement('p');
+  category.classList.add('nameCategoriy');
+  category.innerText= cardMainCategoriy;
+   category.append(cardImg);
+
+   let titleCard = document.createElement('h4');
+   titleCard.innerText = cardTitle;
+   titleCard.classList.add('card-title');
+   
+   let textCard = document.createElement('p');
+   textCard.innerHTML = cardBody;
+   textCard.classList.add('card-text');
+   
+   let cardDate = document.createElement('p');
+   cardDate.innerText = cardMainDate;
+   cardDate.classList.add('card-date'); 
+
+   card.append(cardImg,titleCard,textCard,cardDate);
+  document.getElementById('cardsRow').append(card);
+ }
+
+ 
+
+
+
+
+ //create cotalogue paper//
+
+function getPaperInfo(apiMainId,getApi){
+  let paperTitle = getApi[apiMainId]["title"];
+  let date = new Date(getApi[apiMainId]['date']);
+  let day = date.getDay();
+  let month = date.getMonth();
+  let year =date.getFullYear(); 
+
+  let paperMainDate = day +'.'+month+'.'+year;
+   
+  let news = {
+    title:paperTitle,
+    paperDate:paperMainDate
+  }
+     newsArr.push(news);
+}
+
+
+function createOfPaper(a,b){
+    let cataloguePaper = document.createElement('div');
+    cataloguePaper.classList.add('catalogue-paper');
+
+    let  allViewBtn=document.createElement('div');
+    allViewBtn.classList.add('allViewBtn')
+    allViewBtn.innerText = "BARCHASI";
+    
+    let cataloguePaperTitle = document.createElement('div');
+    cataloguePaperTitle.classList.add('catalogue-paper-title');
+
+    let cataloguePaperTitleText = document.createElement('h4');
+    cataloguePaperTitleText.innerText = 'Boshqa maqolalar';
+    cataloguePaperTitle.append(cataloguePaperTitleText);
+
+    cataloguePaper.append(allViewBtn,cataloguePaperTitle);
+
+     for(j=0;j<3;j++){
+      getPaperInfo(randomNumber(a),b);
+
+
+      let new1 =document.createElement('p');
+      new1.classList.add('catalogue-paper-text','line-clamp');
+      new1.innerText=newsArr[j]['title'];
+      
+      let new1Date=document.createElement('p')
+      new1Date.classList.add('paper-text-date');
+      new1Date.innerText=newsArr[j]['paperDate']
+      
+      cataloguePaper.append(new1,new1Date);
+
+     }
+
+     document.getElementById('papersRow').append(cataloguePaper)
+
+}
+
+
+//random num function//
+
+function randomNumber(max){
+    return Math.floor(Math.random() * max);
+ }
--- header/header-JS.js
+++ header/header-JS.js
@@ -7,32 +7,30 @@ hamburgerBtn.addEventListener('click',helo)
 function helo(){
 
 let leftSectionMain=document.getElementById('leftSectionMain');
-let rightSectionUL=document.getElementById('rightSectionUlID');
+let rightSection=document.getElementById('rightSection');
+let pageMain=document.getElementById('main')
+let pageFooter=document.getElementById('footer')
 
 
-
-if(leftSectionMain.classList.value=="left-main"&&rightSectionUL.classList.value=="rightSectionUl"){
-    leftSectionMain.classList.remove('left-main');
-    leftSectionMain.classList.toggle('navActiv');
-
-    rightSectionUL.classList.remove("rightSectionUl");
-    rightSectionUL.classList.toggle('rightSectionUlMobileVersion');
+if(leftSectionMain.style.display===""){
+    leftSectionMain.style.display="block"
+    rightSection.style.display="block"
+    pageMain.style.display="none"
+    pageFooter.style.display="none"
 
     document.getElementById('uzbek').innerText="Uzbek";
     document.getElementById('russian').innerText="Russian";
     document.getElementById('english').innerText="English";
-
-}
-else{
-    leftSectionMain.classList.toggle('navActiv');
-    leftSectionMain.classList.add('left-main');
-
-    rightSectionUL.classList.toggle('rightSectionUlMobileVersion');
-    rightSectionUL.classList.add("rightSectionUl")
+}else{
+    leftSectionMain.style.display=""
+    rightSection.style.display=""
+    pageMain.style.display="block"
+    pageFooter.style.display="flex"
 
     document.getElementById('uzbek').innerText="Uz";
-document.getElementById('russian').innerText="Py";
-document.getElementById('english').innerText="En";
+    document.getElementById('russian').innerText="Py";
+    document.getElementById('english').innerText="En";
 }
 
 }
+
